694fb5e6b500dac6b39a9c4fa92ec9ce5d1c3547
kingaswag <alexaschiesler@gmail.com>
2026-09-21 11:15:13 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "workout-week-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
